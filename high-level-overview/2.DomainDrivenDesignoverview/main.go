811c75301a4260fbbba5f182bd506cf7961196e6
arjunmalhotra1 <am2cj@virginia.edu>
2023-02-22 06:59:57 -0600

    HighLevelOverview-DomainDrivenDesignOverview

---
+++ high-level-overview/2.DomainDrivenDesignoverview/main.go
@@ -0,0 +1,19 @@
+package main
+
+/*
+See 1.png.
+We want to have our domain model at the center of our application.
+We would want to model our objects after their real world counterparts.
+
+In 1.png
+Circle in the middle, is the domain layer where the objects live, the entities, the value objects,
+the aggregates and the factories. The things that are all about how we structure the core
+business logic if our App. It should have no dependence on what database we pick or an incoming request.
+The purpose of the arrows is the way these things are imported.
+As we can see everything flows out from the center nothing comes in.
+
+*/
+
+func main() {
+
+}
